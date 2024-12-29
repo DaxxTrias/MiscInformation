2823be3f61b4cd039d92a8973a101169f875538e
jahzaer <47497782+jahzaer@users.noreply.github.com>
2024-12-29 08:45:19 +0000

    Add area colour options

--- MiscInformationSettings.cs
+++ MiscInformationSettings.cs
@@ -10,6 +10,7 @@ namespace MiscInformation
         public MiscInformationSettings()
         {
             BackgroundColor = new (Color.FromArgb(255, 0, 0, 0)); // Does nothing.
+            AreaTextColor = new (Color.FromArgb(255, 200, 255, 140));
             XphTextColor = new (Color.FromArgb(220, 190, 130, 255));
             XphGetLeft = new(Color.FromArgb(220, 190, 130, 255));
             TimeLeftColor = new(Color.FromArgb(220, 190, 130, 255));
@@ -19,7 +20,9 @@ namespace MiscInformation
 
         public ToggleNode Enable { get; set; } = new ToggleNode(true);
         public RangeNode<int> DrawXOffset { get; set; } = new RangeNode<int>(0, -150, 150);
+        public ToggleNode UseBuiltInAreaColor { get; set; } = new ToggleNode(true);
         public ColorNode BackgroundColor { get; set; }
+        public ColorNode AreaTextColor { get; set; }
         public ColorNode XphTextColor { get; set; }
         public ColorNode XphGetLeft { get; set; }
         public ColorNode TimeLeftColor { get; set; }
