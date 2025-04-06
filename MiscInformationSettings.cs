66f2226de7d22ee9abfb20db5681c5ef365afdaf
Sean <sean.daxxtrias@gmail.com>
2025-04-06 16:24:53 -0500

    formatting; new menu label, update color slider constructions

--- MiscInformationSettings.cs
+++ MiscInformationSettings.cs
@@ -1,7 +1,8 @@
-﻿using ExileCore2.Shared.Interfaces;
+﻿using ExileCore2.Shared.Attributes;
+using ExileCore2.Shared.Interfaces;
 using ExileCore2.Shared.Nodes;
 using System.Drawing;
-using System.Windows.Forms;
+using System.Numerics;
 
 namespace MiscInformation
 {
@@ -9,13 +10,6 @@ namespace MiscInformation
     {
         public MiscInformationSettings()
         {
-            BackgroundColor = new(Color.FromArgb(255, 0, 0, 0)); // Does nothing.
-            AreaTextColor = new(Color.FromArgb(255, 200, 255, 140));
-            XphTextColor = new(Color.FromArgb(220, 190, 130, 255));
-            XphGetLeft = new(Color.FromArgb(220, 190, 130, 255));
-            TimeLeftColor = new(Color.FromArgb(220, 190, 130, 255));
-            TimerTextColor = new(Color.FromArgb(220, 190, 130, 255));
-            LatencyTextColor = new(Color.FromArgb(220, 190, 130, 255));
             PersistData = new ToggleNode(false);
         }
 
@@ -23,13 +17,14 @@ namespace MiscInformation
         public RangeNode<int> DrawXOffset { get; set; } = new RangeNode<int>(0, -3000, 3000);
         public RangeNode<int> DrawYOffset { get; set; } = new RangeNode<int>(0, -3000, 3000);
         public ToggleNode UseBuiltInAreaColor { get; set; } = new ToggleNode(true);
-        public ColorNode BackgroundColor { get; set; }
-        public ColorNode AreaTextColor { get; set; }
-        public ColorNode XphTextColor { get; set; }
-        public ColorNode XphGetLeft { get; set; }
-        public ColorNode TimeLeftColor { get; set; }
-        public ColorNode TimerTextColor { get; set; }
-        public ColorNode LatencyTextColor { get; set; }
+        [Menu("Persist Data", "If enabled data will not reset on area change")]
         public ToggleNode PersistData { get; set; }
+        public ColorNode BackgroundColor { get; set; } = new ColorNode(Color.FromArgb(255, 0, 0, 0));
+        public ColorNode AreaTextColor { get; set; } = new ColorNode(Color.FromArgb(255, 200, 255, 140));
+        public ColorNode XphTextColor { get; set; } = new ColorNode(Color.FromArgb(220, 190, 130, 255));
+        public ColorNode XphGetLeft { get; set; } = new ColorNode(Color.FromArgb(220, 190, 130, 255));
+        public ColorNode TimeLeftColor { get; set; } = new ColorNode(Color.FromArgb(220, 190, 130, 255));
+        public ColorNode TimerTextColor { get; set; } = new ColorNode(Color.FromArgb(220, 190, 130, 255));
+        public ColorNode LatencyTextColor { get; set; } = new ColorNode(Color.FromArgb(220, 190, 130, 255));
     }
 }
